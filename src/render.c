6b8f130a49b0f59dcefc14523edf0c5e26677fcd
joaosimsic <jpsimsic@hotmail.com>
2026-04-23 22:36:25 -0300

    refac: centering game

--- src/render.c
+++ src/render.c
@@ -1,13 +1,26 @@
 #include "render.h"
+#include "common.h"
 #include "player.h"
 #include "wfc.h"
+#include <asm-generic/ioctls.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
 
 void render_frame(void) {
-  printf("\033[2J\033[H");
+  struct winsize ws;
+  ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws);
+
+  int oy = (ws.ws_row - MAP_HEIGHT) / 2;
+  int ox = (ws.ws_col - MAP_WIDTH) / 2;
+
+  printf("\033[2J");
 
   for (int i = 0; i < MAP_HEIGHT * MAP_WIDTH; i++) {
     int y = i / MAP_WIDTH;
     int x = i % MAP_WIDTH;
+
+    if (x == 0)
+      printf("\033[%d;%dH", oy + y + 1, ox + 1);
 
     if (player.y == y && player.x == x) {
       printf("\033[38;5;226m@\033[0m");
